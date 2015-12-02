9e7dbef4d86f3dc8c2147f7207462821689e5710
Craig McCown <craigrmccown@gmail.com>
2015-12-02 18:57:22 -0500

    displaying sections

--- src/app/selection/selection.controllers.js
+++ src/app/selection/selection.controllers.js
@@ -1,5 +1,45 @@
 angular.module('gg.app')
     .controller('SelectionCtrl', function($scope, Courses, CompletedCourses, AvailableCourses) {
         $scope.courses = Courses;
-        $scope.availableCourses = AvailableCourses;
+        $scope.availableCourses = [];
+        $scope.selectedSections = [];
+
+        AvailableCourses.forEach(
+            function(course) {
+                var available = {
+                    course: course,
+                    sections: []
+                };
+
+                course.sections.forEach(
+                    function(section) {
+                        available.sections.push({
+                            section: section,
+                            selected: false,
+                            available: true
+                        });
+                    });
+
+                $scope.availableCourses.push(available);
+            });
+
+        $scope.selectSection = function(section) {
+            $scope.selectedSections.push(section);
+            section.selected = true;
+            section.available = false;
+
+            /* TODO filter available list */
+        }
+
+        $scope.deselectSection = function(section) {
+            var sectionIdx = _.findIndex(
+                $scope.selectedSections,
+                function(selected) {
+                    return selected.section.id == section.section.id;
+                });
+
+            $scope.selectedSections.splice(sectionIdx, 1);
+            section.selected = false;
+            section.available = true;
+        }
     });
